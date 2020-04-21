6bd4679351bca74d417d210be16a239d789757b9
Robin Blanchard <robin.blanchard00@gmail.com>
2020-04-21 18:59:05 +0200

    Customized Recipe Admin display

--- backend/re7/recipes/admin.py
+++ backend/re7/recipes/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from re7.recipes.models import Recipe
 
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+    def get_difficulty(self, obj):
+        return obj.get_difficulty_display()
+    get_difficulty.short_description = "difficulty"
+
+    list_display = ("name", "get_difficulty", "prep_time")
+
+
 # Register your models here.
-admin.site.register(Recipe, admin.ModelAdmin)
+admin.site.register(Recipe, RecipeAdmin)
--- backend/re7/recipes/models.py
+++ backend/re7/recipes/models.py
@@ -5,16 +5,19 @@ from django.db import models
 
 
 class Recipe(models.Model):
-    DIFFICULTY_LEVELS = [(1, "Simple"),
-                         (2, "Modéré"),
-                         (3, "Difficile")]
+    EASY = '1'
+    MEDIUM = '2'
+    HARD = '3'
+    DIFFICULTY_LEVELS = ((EASY, "Simple"),
+                         (MEDIUM, "Modéré"),
+                         (HARD, "Difficile"))
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     creation_date = models.DateField(auto_now_add=True, editable=False)
     update_date = models.DateField(auto_now=True, editable=False)
 
     name = models.CharField(max_length=50)
-    difficulty = models.IntegerField(choices=DIFFICULTY_LEVELS)
+    difficulty = models.CharField(max_length=1, choices=DIFFICULTY_LEVELS)
     prep_time = models.PositiveSmallIntegerField()
     bake_time = models.PositiveSmallIntegerField()
     total_time = models.PositiveSmallIntegerField()
