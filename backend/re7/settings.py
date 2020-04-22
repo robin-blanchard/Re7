c070e3dca3c38d710b82fc5268aadc25e884d475
Robin Blanchard <robin.blanchard00@gmail.com>
2020-04-22 15:15:31 +0200

    Add ALLOWED_HOSTS to .env

--- backend/re7/settings.py
+++ backend/re7/settings.py
@@ -25,7 +25,7 @@ if os.path.exists(dotenv):
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env("ALLOWED_HOSTS", default=[])
 
 
 # Application definition
