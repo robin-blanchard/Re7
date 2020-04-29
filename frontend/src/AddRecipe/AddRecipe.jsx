017c0c32c53e637a16ded505f8638b5dd55553e2
Robin Blanchard <robin.blanchard00@gmail.com>
2020-04-29 16:24:35 +0200

    Added ingredients to form

--- frontend/src/AddRecipe/AddRecipe.jsx
+++ frontend/src/AddRecipe/AddRecipe.jsx
@@ -2,14 +2,42 @@ import React, { useState, useEffect } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 function AddRecipe() {
   const [prepTime, setPrepTime] = useState(0);
   const [bakeTime, setBakeTime] = useState(0);
   const [totalTime, setTotalTime] = useState(0);
+  const [ingredients, setIngredients] = useState([
+    {
+      quantity: 0,
+      unit: "kg",
+      product: "",
+    },
+  ]);
 
-  const onTimeChange = (e, setValue) => {
-    if (e.target.value < 0) {
+  const addIngredient = () => {
+    setIngredients([
+      ...ingredients,
+      {
+        quantity: 0,
+        unit: "kg",
+        product: "",
+      },
+    ]);
+  };
+  const deleteIngredient = (indexToRemove) => {
+    setIngredients(ingredients.filter((_, idx) => idx !== indexToRemove));
+  };
+  const updateIngredient = (ingredientIndex, fieldToUpdate, value) => {
+    const newIngredients = ingredients.slice();
+    newIngredients[ingredientIndex][fieldToUpdate] = value;
+    setIngredients(newIngredients);
+  };
+
+  const onChangeShouldBePositive = (e, setValue) => {
+    if (e.target.value === "") {
+    } else if (e.target.value < 0) {
       setValue(0);
     } else {
       setValue(Math.round(e.target.value));
@@ -20,8 +48,14 @@ function AddRecipe() {
     setTotalTime(prepTime + bakeTime);
   }, [prepTime, bakeTime]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    console.log(data);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group controlId="form.ControlName">
         <Form.Label>Nom de la recette</Form.Label>
         <Form.Control placeholder="Purée" />
@@ -35,6 +69,7 @@ function AddRecipe() {
           <option value="3">Difficile</option>
         </Form.Control>
       </Form.Group>
+
       <Form.Row>
         <Form.Group as={Col} controlId="form.ControlPrepTime">
           <Form.Label>Temps de préparation</Form.Label>
@@ -42,7 +77,7 @@ function AddRecipe() {
             type="number"
             step="1"
             value={prepTime}
-            onChange={(e) => onTimeChange(e, setPrepTime)}
+            onChange={(e) => onChangeShouldBePositive(e, setPrepTime)}
           />
         </Form.Group>
 
@@ -52,20 +87,68 @@ function AddRecipe() {
             type="number"
             step="1"
             value={bakeTime}
-            onChange={(e) => onTimeChange(e, setBakeTime)}
+            onChange={(e) => onChangeShouldBePositive(e, setBakeTime)}
           />
         </Form.Group>
 
         <Form.Group as={Col} controlId="form.ControlBakeTime">
           <Form.Label>Temps total</Form.Label>
-          <Form.Control
-            type="number"
-            disabled
-            value={totalTime}
-            onChange={(e) => onTimeChange(e, setBakeTime)}
-          />
+          <Form.Control type="number" disabled value={totalTime} />
         </Form.Group>
       </Form.Row>
+      <Form.Row>
+        <Form.Label>Ingrédients</Form.Label>
+        <Button onClick={addIngredient}>Plus</Button>
+      </Form.Row>
+
+      {ingredients.map((ingredientItem, idx) => (
+        <Form.Row key={idx}>
+          <Form.Group as={Col} md={2}>
+            <Form.Control
+              type="number"
+              value={ingredientItem.quantity}
+              onChange={(e) => {
+                onChangeShouldBePositive(e, (value) => {
+                  updateIngredient(idx, "quantity", value);
+                });
+              }}
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} md={1}>
+            <Form.Control
+              as="select"
+              value={ingredientItem.unit}
+              onChange={(e) => {
+                updateIngredient(idx, "unit", e.target.value);
+              }}
+            >
+              <option>g</option>
+              <option>kg</option>
+            </Form.Control>
+          </Form.Group>
+
+          <p style={{ alignSelf: "center" }}>de</p>
+
+          <Form.Group as={Col} md={3}>
+            <Form.Control
+              value={ingredientItem.product}
+              onChange={(e) => {
+                updateIngredient(idx, "product", e.target.value);
+              }}
+            />
+          </Form.Group>
+
+          <Button
+            onClick={(e) => {
+              deleteIngredient(idx);
+            }}
+          >
+            Delete
+          </Button>
+        </Form.Row>
+      ))}
+      <Button type="submit">Submit</Button>
     </Form>
   );
 }
