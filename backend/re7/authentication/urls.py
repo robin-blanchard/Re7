e346e7f58acf123d3920c32f6ffd21a84dc6635e
Robin Blanchard <robin.blanchard00@gmail.com>
2020-05-07 15:56:21 +0200

    Added Customer creation view

--- backend/re7/authentication/urls.py
+++ backend/re7/authentication/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
+from re7.authentication.views import CustomUserCreate
+
 urlpatterns = [
+    path('user/create/', CustomUserCreate.as_view(), name="create_user"),
     path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/re7/authentication/views.py
+++ backend/re7/authentication/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+
+from re7.authentication.models import CustomUser
+from re7.authentication.serializers import CustomUserSerializer
 
 # Create your views here.
+
+
+class CustomUserCreate(generics.CreateAPIView):
+    permission_classes = (permissions.AllowAny,)
+
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
