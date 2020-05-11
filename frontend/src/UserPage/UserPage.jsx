f7b67354fbbba6dccc0895d52a966f05ff7ead9d
Robin Blanchard <robin.blanchard00@gmail.com>
2020-05-11 18:33:48 +0200

    Added user's recipes to his profile page

--- frontend/src/UserPage/UserPage.jsx
+++ frontend/src/UserPage/UserPage.jsx
@@ -1,10 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
 import Axios from "axios";
 
+import RecipeGrid from "../RecipeGrid/RecipeGrid";
+
 const UserPage = (props) => {
   const { username } = useParams();
-  return <h1>Welcome to {username}'s profile</h1>;
+  const [recipesItems, setRecipesItems] = useState([]);
+
+  useEffect(() => {
+    const CancelToken = Axios.CancelToken;
+    const source = CancelToken.source();
+
+    const url = process.env.REACT_APP_BACKEND_URL + "api/user/" + username;
+
+    const loadData = () => {
+      Axios.get(url, { cancelToken: source.token })
+        .then((response) => {
+          setRecipesItems(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+
+    loadData();
+    return () => {
+      source.cancel();
+    };
+  }, []);
+
+  return (
+    <Fragment>
+      <h1>Welcome to {username}'s page</h1>
+      <RecipeGrid items={recipesItems} />
+    </Fragment>
+  );
 };
 
 export default UserPage;
