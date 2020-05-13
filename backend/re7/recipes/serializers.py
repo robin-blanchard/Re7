2d2f57b3ae30da5d80a376e670a8820feaad5227
Robin Blanchard <robin.blanchard00@gmail.com>
2020-05-13 17:31:13 +0200

    Implemented recipe update view and serializer

--- backend/re7/recipes/serializers.py
+++ backend/re7/recipes/serializers.py
@@ -56,3 +56,41 @@ class RecipeSerializer(serializers.ModelSerializer):
             Instruction.objects.create(recipe=recipe, **instruction_data)
 
         return recipe
+
+    def update(self, instance, validated_data):
+        ingredients_set_data = validated_data.pop("ingredients")
+        instructions_set_data = validated_data.pop("instructions")
+        creater = validated_data.pop("creater", None)
+        photo_url = validated_data.pop("photo_url", None)
+
+        instance.name = validated_data.get("name", instance.name)
+        instance.difficulty = validated_data.get(
+            "difficulty", instance.difficulty)
+        instance.prep_time = validated_data.get(
+            "prep_time", instance.prep_time)
+        instance.bake_time = validated_data.get(
+            "bake_time", instance.bake_time)
+        instance.total_time = validated_data.get(
+            "total_time", instance.total_time)
+
+        if photo_url is not None:
+            instance.photo = photo_url
+
+        Ingredient.objects.filter(recipe=instance).delete()
+        for ingredient_data in ingredients_set_data:
+            product_data = ingredient_data.pop("product")
+            product, _ = Product.objects.get_or_create(**product_data)
+
+            Ingredient.objects.create(
+                recipe=instance, product=product, **ingredient_data)
+
+        for instruction_data in instructions_set_data:
+            order = instruction_data.get("order")
+            instruction, _ = Instruction.objects.get_or_create(
+                recipe=instance, order=order)
+
+            instruction.text = instruction_data.get("text")
+            instruction.save()
+
+        instance.save()
+        return instance
--- backend/re7/recipes/views.py
+++ backend/re7/recipes/views.py
@@ -13,7 +13,7 @@ class RecipesListView(generics.ListCreateAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
 
-    def post(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
         data = request.data
 
         if isinstance(data, QueryDict):
@@ -38,9 +38,37 @@ class RecipesListView(generics.ListCreateAPIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class RecipeDetailView(generics.RetrieveAPIView):
+class RecipeDetailView(generics.RetrieveUpdateAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+    def update(self, request, *args, **kwargs):
+        data = request.data
+
+        if isinstance(data, QueryDict):
+            data = data.dict()
+
+        if isinstance(data["ingredients"], str):
+            data["ingredients"] = json.loads(data["ingredients"])
+
+        if isinstance(data["instructions"], str):
+            data["instructions"] = json.loads(data["instructions"])
+
+        if "photo" in data and isinstance(data["photo"], str):
+            photo_url = data.pop("photo")
+        else:
+            photo_url = None
+
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer, photo_url=photo_url)
+
+        return Response(serializer.data)
+
+    def perform_update(self, serializer, photo_url):
+        serializer.save(photo_url=photo_url)
 
 
 class ProductsListView(generics.ListCreateAPIView):
