4212fd3d2729310848aaddfd38628503a40e57f4
Robin Blanchard <robin.blanchard00@gmail.com>
2020-05-26 19:12:13 +0200

    Main Recipes Page now fetches recipes 6 by 6

--- frontend/src/MainRecipesPage/MainRecipesPage.jsx
+++ frontend/src/MainRecipesPage/MainRecipesPage.jsx
@@ -6,27 +6,56 @@ import RecipeGrid from "../RecipeGrid/RecipeGrid";
 
 function MainRecipesPage() {
   const [recipesItems, setRecipesItems] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const handlePageBottom = () => {
+    setIsFetching(true);
+  };
+
+  const checkPageBottom = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop >=
+      document.documentElement.offsetHeight
+      // document.body.clientHeight < window.innerHeight
+    ) {
+      handlePageBottom();
+    }
+  };
 
   useEffect(() => {
+    checkPageBottom();
+    window.addEventListener("scroll", checkPageBottom);
+    return () => window.removeEventListener("scroll", checkPageBottom);
+  }, [recipesItems]);
+
+  const fetchMoreListItems = () => {
     const CancelToken = Axios.CancelToken;
     const source = CancelToken.source();
+    const offset = recipesItems.length;
+    const limit = 6;
 
-    const loadData = () => {
-      axiosInstanceNoAuth
-        .get("api/recipes", { cancelToken: source.token })
-        .then((response) => {
-          setRecipesItems(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
+    axiosInstanceNoAuth
+      .get("api/recipes?limit=" + limit + "&offset=" + offset, {
+        cancelToken: source.token,
+      })
+      .then((response) => {
+        setIsFetching(false);
+        setRecipesItems([...recipesItems, ...response.data.results]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-    loadData();
     return () => {
       source.cancel();
     };
-  }, []);
+  };
+
+  useEffect(() => {
+    if (!isFetching) return;
+
+    return fetchMoreListItems();
+  }, [isFetching]);
 
   return (
     <Fragment>
