05cdefb75e3e4ec7ebcdcd8872892e17e0f7a737
Robin Blanchard <robin.blanchard00@gmail.com>
2020-05-27 18:38:34 +0200

    Added search users view

--- backend/re7/authentication/urls.py
+++ backend/re7/authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
-from re7.authentication.views import CustomUserCreate
+from re7.authentication.views import CustomUserCreate, CustomUsersList
 
 urlpatterns = [
     path('user/create/', CustomUserCreate.as_view(), name="create_user"),
+    path('users', CustomUsersList.as_view(), name="users_list"),
     path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/re7/authentication/views.py
+++ backend/re7/authentication/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from rest_framework.pagination import LimitOffsetPagination
 
 from re7.authentication.models import CustomUser
 from re7.authentication.serializers import CustomUserSerializer
+
 
 # Create your views here.
 
@@ -11,3 +13,13 @@ class CustomUserCreate(generics.CreateAPIView):
 
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+
+
+class CustomUsersList(generics.ListAPIView):
+    permission_classes = (permissions.AllowAny,)
+
+    serializer_class = CustomUserSerializer
+    search_fields = ["username"]
+    filter_backends = (filters.SearchFilter,)
+    queryset = CustomUser.objects.all().order_by("username")
+    pagination_class = LimitOffsetPagination
